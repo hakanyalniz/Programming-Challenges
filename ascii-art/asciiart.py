03d265d135741ead891d4901472db3a9958d1f97
hakanyalniz <hakan_yalniz@live.com>
2025-03-02 19:36:21 +0300

    Changed the brightness logic by using integer division instead of modulus operator.

--- ascii-art/asciiart.py
+++ ascii-art/asciiart.py
@@ -5,7 +5,7 @@ ASCII_FONTS = [" " ,".", ";", "o", "O", "a", "A", "#"]
 ASCII_NUMBERS = len(ASCII_FONTS)
 
 def main():
-    image = Image.open("./151.jpg")
+    image = Image.open("./test_small.png")
 
     generate_ascii_image(resize_image(image))
 
@@ -23,8 +23,8 @@ def generate_ascii_image(originalImage):
     font = ImageFont.truetype("arial.ttf", 8)
 
     # Loop through each pixel, get the pixel value
-    for y in range(round(originalHeight)):
-        for x in range(round(originalWidth)):
+    for y in range(originalHeight):
+        for x in range(originalWidth):
             pixel = originalImage.getpixel((x, y))
             # get brightness of pixel, then get the proper ASCII font for that brightness
             ASCII = generate_ascii_font(get_brightness(pixel))
@@ -40,24 +40,28 @@ def resize_image(image):
     small_image = image.resize((originalWidth // 8, originalHeight // 8))
     return small_image.resize(image.size).convert("RGB")
 
+
 def generate_ascii_font(brightness):
     '''Given brightness value, return an appropriate ASCII'''
 
-    # Loop through each ascii font, since each brightness maps to each font
-    # from 0 to the end, if we find the correct index, return that ascii
+    # each brightness maps to each font
+    # from 0 to the end, return that ascii
     # [" " ,".", ";", "o" ...
     #  0     1    2    3 
     # As in, each level of 0, 1, 2 and so on, corresponds to brightness level
     # and each of the ASCII index, match those levels
-    # range gives 
-    for index in range(ASCII_NUMBERS):
-        if (brightness == index):
-            return ASCII_FONTS[index]
+    return ASCII_FONTS[brightness]
+
 
 def get_brightness(pixel):
     '''Return the rounded brightness of the pixel modulated to total number of available font.'''
 
+    # Calculate average brightness of the pixel (R + G + B) / 3
+    brightness = sum(pixel[:3]) / 3
+
     # Minus one, so it returns 0 to 7, instead of 0 to 8
-    return round((sum(pixel[:3]) / 3) % (ASCII_NUMBERS - 1))
+    # Map brightness (0 to 255) to the ASCII font range (0 to ASCII_NUMBERS-1)
+    return round(brightness * (ASCII_NUMBERS - 1) // 255)
+
 
 main()
