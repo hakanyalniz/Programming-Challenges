fac1f817f30992e67a2fed620daaf1bb1a1ed0ae
hakanyalniz <hakan_yalniz@live.com>
2025-03-02 21:47:22 +0300

    Added color to the ASCII art. Might add more themes later on.

--- ascii-art/asciiart.py
+++ ascii-art/asciiart.py
@@ -1,12 +1,12 @@
 '''Create a program that turns ordinary images to ASCII art'''
 from PIL import Image, ImageDraw, ImageFont
 
-ASCII_FONTS = ["#", "A", "a", "O", "o", ";", ".", " "]
-ASCII_NUMBERS = len(ASCII_FONTS)
+ASCII_FONTS = ["#", "@", "%", "A", "a", "O", "o", ";", ".", " "]
+ASCII_NUMBERS = len(ASCII_FONTS)    
 
 def main():
     # image = Image.open("./test.png")
-    image = Image.open("./151.jpg")
+    image = Image.open("./test.png")
 
     generate_ascii_image(resize_image(image))
 
@@ -23,17 +23,23 @@ def generate_ascii_image(originalImage):
     # Initialize ImageDraw, which we will use to draw ASCII
     draw = ImageDraw.Draw(ASCII_Image)
     # Specify the font and size
-    font = ImageFont.truetype("arial.ttf", 14)
+    font = ImageFont.truetype("./dejavu-sans-mono/DejaVuSansMono.ttf", 14)
 
     # Loop through each pixel, get the pixel value
     for y in range(originalHeight):
+        # ASCII_ROW = ""
         for x in range(originalWidth):
             pixel = originalImage.getpixel((x, y))
+            r, g, b = pixel[:3]  # Extract RGB values (ignore alpha if present)
 
             # get brightness of pixel, then get the proper ASCII font for that brightness
             ASCII = generate_ascii_font(get_brightness(pixel))
-            draw.text((x * 8, y * 8), ASCII, fill="black", font=font)
-    
+            draw.text((x * 8, y * 8), ASCII, fill=(r, g, b), font=font)
+            # ASCII_ROW += ASCII
+
+        # draw.text((0, y * 8), ASCII_ROW, fill="black", font=font)
+            
+    ASCII_Image.show()
     ASCII_Image.save("ascii_image.png")
 
 
@@ -58,6 +64,8 @@ def generate_ascii_font(brightness):
 
 
 def get_brightness(pixel):
+    import math
+
     '''Return the rounded brightness of the pixel modulated to total number of available font.'''
 
     # Calculate average brightness of the pixel (R + G + B) / 3
@@ -65,7 +73,7 @@ def get_brightness(pixel):
 
     # Minus one, so it returns 0 to 7, instead of 0 to 8
     # Map brightness (0 to 255) to the ASCII font range (0 to ASCII_NUMBERS-1)
-    return round(brightness * (ASCII_NUMBERS - 1) // 255)
+    return math.floor(brightness * (ASCII_NUMBERS - 1) // 255)
 
 
 main()
