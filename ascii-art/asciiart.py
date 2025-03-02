bf9dfcd1d14ac4b185f4788b5bcc942413841c92
hakanyalniz <hakan_yalniz@live.com>
2025-03-03 01:38:05 +0300

    Added some global variables to make adjustments easier.

--- ascii-art/asciiart.py
+++ ascii-art/asciiart.py
@@ -1,12 +1,17 @@
 '''Create a program that turns ordinary images to ASCII art'''
 from PIL import Image, ImageDraw, ImageFont
 
-ASCII_FONTS = ["#", "@", "%", "A", "a", "O", "o", ";", ".", " "]
+ASCII_FONTS = ["#", "@", "%", "A", "a", "O", "o", "W", "w", "N", "n", ";", ".", " "]
+# ASCII_FONTS = [" ", ".", ";", "n", "N", "w", "W", "o", "O", "a", "A", "%", "@", "#"]
+# ASCII_FONTS = ["#", "@", "%", "A", "a", "O", "o", ";", ".", " "]
 ASCII_NUMBERS = len(ASCII_FONTS)    
+# these two values can be played with to adjust the ASCII size and definition in the image
+FONT_SIZE = 10
+ASCII_SPACING = 8
 
 def main():
     # image = Image.open("./test.png")
-    image = Image.open("./test.png")
+    image = Image.open("./test.jpg")
 
     generate_ascii_image(resize_image(image))
 
@@ -18,26 +23,33 @@ def generate_ascii_image(originalImage):
     originalWidth, originalHeight = originalImage.size
     # The original image was resized to become small by factor of 8, we take that size
     # and increase it by 8 to get original size
-    ASCII_Image = Image.new("RGB", (originalWidth * 8, originalHeight * 8), "white")
+    ASCII_Image = Image.new("RGB", (originalWidth * ASCII_SPACING, originalHeight * ASCII_SPACING), "white")
 
     # Initialize ImageDraw, which we will use to draw ASCII
     draw = ImageDraw.Draw(ASCII_Image)
     # Specify the font and size
-    font = ImageFont.truetype("./dejavu-sans-mono/DejaVuSansMono.ttf", 14)
+    font = ImageFont.truetype("./dejavu-sans-mono/DejaVuSansMono.ttf", FONT_SIZE)
 
     # Loop through each pixel, get the pixel value
     for y in range(originalHeight):
-        # ASCII_ROW = ""
+        ASCII_ROW = ""
+        ASCII_colors = []
         for x in range(originalWidth):
             pixel = originalImage.getpixel((x, y))
             r, g, b = pixel[:3]  # Extract RGB values (ignore alpha if present)
 
             # get brightness of pixel, then get the proper ASCII font for that brightness
             ASCII = generate_ascii_font(get_brightness(pixel))
-            draw.text((x * 8, y * 8), ASCII, fill=(r, g, b), font=font)
-            # ASCII_ROW += ASCII
 
-        # draw.text((0, y * 8), ASCII_ROW, fill="black", font=font)
+            # add the ASCII and its color so we can later on draw them together
+            ASCII_ROW += ASCII
+            ASCII_colors.append((r, g, b))
+
+            # draw.text((x * 8, y * 8), ASCII, fill=(r, g, b), font=font)
+
+        # Draw the entire row with colored characters
+        for x, char in enumerate(ASCII_ROW):
+            draw.text((x * ASCII_SPACING, y * ASCII_SPACING), char, fill=ASCII_colors[x], font=font)
             
     ASCII_Image.show()
     ASCII_Image.save("ascii_image.png")
@@ -47,7 +59,7 @@ def resize_image(image):
     '''Resize image to half'''
 
     originalWidth, originalHeight = image.size
-    small_image = image.resize((originalWidth // 8, originalHeight // 8))
+    small_image = image.resize((originalWidth // ASCII_SPACING, originalHeight // ASCII_SPACING))
     return small_image.convert("RGB")
 
 
