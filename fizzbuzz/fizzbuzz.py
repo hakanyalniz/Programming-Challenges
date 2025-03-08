38937da36f0980ae0df42c65df67434197643815
hakanyalniz <hakan_yalniz@live.com>
2025-03-08 23:47:26 +0300

    Made an improved version of FizzBuzz.

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -4,8 +4,11 @@
 # For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
 def main():
+    improvedFizzBuzz()
+
+
+def ordinaryFizzBuzz():
     for i in range(1, 101):
-        
         if (i % 3) == 0 and (i % 5) == 0:
             print("FizzBuzz")
         elif (i % 3) == 0:
@@ -14,6 +17,24 @@ def main():
             print("Buzz")
         else:
             print(i)
+
+
+def improvedFizzBuzz():
+    fizzRules = {
+    15: "FizzBuzz",
+    5: "Buzz",
+    3: "Fizz",
+    }
+    for i in range(101):
+        for rule in fizzRules:
+            if i % rule == 0:
+                print(fizzRules[rule])
+                break
+            elif rule == 3:
+                print(i) 
+            
+
+
 
 if __name__ == "__main__":
     main()
