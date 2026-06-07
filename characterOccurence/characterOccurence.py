9a34998f8654c8c82c5d92769e3102f6771fcec0
hakanyalniz <hakan_yalniz@live.com>
2026-06-07 17:40:01 +0300

    Implemented the toggle check. The program works now as intended.

--- characterOccurence/characterOccurence.py
+++ characterOccurence/characterOccurence.py
@@ -1,6 +1,10 @@
 toggle_capital: bool = True if input("Count capital letters as separate instances?\n").lower() == "true" else False
 
-user_name: str = input("Enter string: ")
+if toggle_capital:
+    user_name: str = input("Enter string: ")
+else:
+    user_name: str = input("Enter string: ").lower()
+
 count_string: dict[str, int] = {}
 
 
@@ -9,6 +13,7 @@ for i in range(len(user_name)):
         count_string[user_name[i]] += count_string[user_name[i]]
     else:
         count_string[user_name[i]] = 1
+
 
 
 print(count_string)
