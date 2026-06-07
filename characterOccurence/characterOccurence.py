dedfd38645e85f07b9eb8ec84e8ee003915e7f68
hakanyalniz <hakan_yalniz@live.com>
2026-06-07 17:36:27 +0300

    Fixed a bug with incrementing. Added a variable and basic check to switch to determine counting capital characters or not.

--- characterOccurence/characterOccurence.py
+++ characterOccurence/characterOccurence.py
@@ -1,10 +1,12 @@
+toggle_capital: bool = True if input("Count capital letters as separate instances?\n").lower() == "true" else False
+
 user_name: str = input("Enter string: ")
 count_string: dict[str, int] = {}
 
 
 for i in range(len(user_name)):
     if user_name[i] in count_string:
-        count_string[user_name[i]] = +count_string[user_name[i]]
+        count_string[user_name[i]] += count_string[user_name[i]]
     else:
         count_string[user_name[i]] = 1
 
