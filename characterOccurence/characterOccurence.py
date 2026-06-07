0dd169c30d3897c5106aea48f4b28e3c3fce8de0
hakanyalniz <hakan_yalniz@live.com>
2026-06-07 18:35:20 +0300

    Fixed the sorting and fully implemented it. Duplicates were created because it was checking each item in the list, this was fixed. Edge cases may still remain.

--- characterOccurence/characterOccurence.py
+++ characterOccurence/characterOccurence.py
@@ -15,18 +15,18 @@ for i in range(len(user_name)):
     else:
         count_string[user_name[i]] = 1
 
-print("count_string", count_string)
 
 for key, value in count_string.items():
-    # print(key, value)
     if len(sorted_string_list) == 0: # If no element in list, just insert it into index 0
             sorted_string_list.insert(0, (key, value))
             continue
     
-    for element in sorted_string_list[:]: # element here is a tuple like ("h", 2), we create a shallow copy or else it will create infinite loop
-        # print("sorted_string_list", sorted_string_list)
-        
+    for element in sorted_string_list[:]: # element here is a tuple like ("h", 2), we create a shallow copy or else it will create infinite loop        
         element_index: int = sorted_string_list.index(element)
+
+        if (key, value) in sorted_string_list: # If the sorted pair is already in list, we do not need to continue checking,
+            # if we do, then it will also add the item before every other item after the first one (which is the biggest number)
+            continue
 
         if value > element[1]: # If the value is greater than the value present in list, put it before the element
             sorted_string_list.insert(element_index, (key, value))
@@ -35,9 +35,7 @@ for key, value in count_string.items():
 
 
 
-
-# print(count_string)
-# print("Sorted", sorted_string_list)
+print("Sorted", sorted_string_list)
 
 # Look over the dictionary one by one
 # Get the largest number and put it into list
