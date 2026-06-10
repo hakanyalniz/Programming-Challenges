346bc3a141b2107b1b9e46c83772c7bcd0192ad5
hakanyalniz <hakan_yalniz@live.com>
2026-06-10 19:39:25 +0300

    Used a formatter to format the code.

--- passwordGenerate/helpers.py
+++ passwordGenerate/helpers.py
@@ -9,31 +9,33 @@ def get_password_length_input() -> int:
         raw_length: str = input("Password length: ")
         if raw_length == "":
             return 16
-        
+
         try:
             int_length: int = int(raw_length)
-            if (int_length < 12):
+            if int_length < 12:
                 print("Please pick a greater length. \n")
                 continue
             return int_length
         except ValueError:
             print("Please enter a number.\n")
 
+
 def include_special_toggle() -> bool:
     """
-    A function that returns true or false to toggle special character list. 
+    A function that returns true or false to toggle special character list.
     The user can only enter true or false. Only accepts those two inputs.
     """
-        
+
     while True:
         raw_input = input("Include special characters: ")
-        try:   
+        try:
             if raw_input.lower() == "yes":
                 return True
             elif raw_input.lower() == "no" or raw_input == "":
                 return False
         except ValueError:
             print("Please enter Yes or No")
+
 
 def write_password_toFile(generated_password: str):
     """
@@ -45,31 +47,41 @@ def write_password_toFile(generated_password: str):
     with open("passwords.txt", "a", encoding="utf-8") as file:
         file.write(f"{generated_password} , {raw_input}\n")
 
+
 def read_passwords():
     """
     Opens a password.txt file and reads from it, then prints them on screen.
+    Exit the program after showing the passwords.
     """
 
     while True:
         raw_input = input("Read your passwords?: ")
-        try:   
+        try:
             if raw_input.lower() == "yes":
-                    with open("passwords.txt", "r", encoding="utf-8") as file:
-                        print("\n\n")
-                        for line in file:
-                            # strip() removes the trailing newline character (\n)
-                            print(line.strip())
+                with open("passwords.txt", "r", encoding="utf-8") as file:
                     print("\n\n")
-                    exit()
-                            
+                    for line in file:
+                        # strip() removes the trailing newline character (\n)
+                        print(line.strip())
+                print("\n\n")
+                exit()
+
             elif raw_input.lower() == "no" or raw_input == "":
                 return False
         except ValueError:
             print("Please enter Yes or No")
 
 
+def delete_password():
+    """
+    The user can select a password based on row and delete it.
+    """
 
-# Refactor all the code here so the inputs and so on are in one main function
-# while the utility is left to the functions themselves
+    with open("passwords.txt", "r", encoding="utf-8") as file:
+        print("\n\n")
+        for line in file:
+            # strip() removes the trailing newline character (\n)
+            print(line.strip())
+
+
 # Where the user can chose to either enter new password, read from it, see them, delete them and so on
-# Refactor to change and overhaul the UI
--- passwordGenerate/passwordGenerate.py
+++ passwordGenerate/passwordGenerate.py
@@ -1,24 +1,84 @@
 import random
-from helpers import include_special_toggle, get_password_length_input, write_password_toFile, read_passwords
 
+from helpers import (
+    get_password_length_input,
+    include_special_toggle,
+    read_passwords,
+    write_password_toFile,
+)
 
-input("\n\nYou will be asked the following. You can also press enter for default:\n" \
-"To include special characters in password generation or not. (Yes or No) (!, @, # and so on) \n" \
-"Password length. Minimum is 12 length.\n\n\n" \
-"You can also access your current passwords. \n" \
-"Press enter to continue.\n" \
+input(
+    "\n\nYou will be asked the following. You can also press enter for default:\n"
+    "To include special characters in password generation or not. (Yes or No) (!, @, # and so on) \n"
+    "Password length. Minimum is 12 length.\n\n\n"
+    "You can also access your current passwords. \n"
+    "Press enter to continue.\n"
 )
 
 read_passwords()
 
 # If we toggle true, use 1 or else use 0. Then add it to the random number upper limit. This will include symbols list or not
-special_char_toggle = 1 if include_special_toggle() else 0 
+special_char_toggle = 1 if include_special_toggle() else 0
 password_length: int = get_password_length_input()
 final_password: str = ""
 
 
-lower_alphabet: list[str] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-upper_alphabet: list[str] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+lower_alphabet: list[str] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+upper_alphabet: list[str] = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
 numbers: list[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 symbols: list[str] = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="]
 
@@ -26,19 +86,19 @@ symbols: list[str] = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="
 for index in range(password_length):
     match random.randint(0, 2 + special_char_toggle):
         case 0:
-            final_password += lower_alphabet[random.randint(0,20)]
+            final_password += lower_alphabet[random.randint(0, 20)]
         case 1:
-            final_password += upper_alphabet[random.randint(0,20)]
+            final_password += upper_alphabet[random.randint(0, 20)]
         case 2:
-            final_password += str(numbers[random.randint(0,9)])
+            final_password += str(numbers[random.randint(0, 9)])
         case 3:
             final_password += symbols[random.randint(0, 11)]
-        case _: pass
+        case _:
+            pass
 
 write_password_toFile(final_password)
 
 print(final_password)
-
 
 
 # store selected passwords in a notepad
